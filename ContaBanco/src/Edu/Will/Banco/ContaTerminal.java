091e4d183ee603438e8ae45f27d0d8f372e1e881
will <willpro2806@gmail.com>
2024-09-11 08:26:47 -0300

    Resolução do desafio 1, um simulador de banco em um terminal

---
+++ ContaBanco/src/Edu/Will/Banco/ContaTerminal.java
@@ -0,0 +1,38 @@
+package Edu.Will.Banco;
+
+import java.util.Scanner;
+
+public class ContaTerminal {
+    
+    public static String nomeCliente;
+    public static String agencia;
+    public static int numero;
+    public static double saldo;
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        /*
+         * Atribuindo valores pelo terminal:
+         * nomeCliente = args[0];
+         * agencia = args[1];
+         * numero = Integer.valueOf(args[2]);
+         * saldo = Double.valueOf(args[3]);
+         * 
+        */
+
+        System.out.printf("Digite o nome do cliente: ");
+        nomeCliente = sc.nextLine();
+        System.out.printf("Digite a agencia do cliente: ");
+        agencia = sc.nextLine();
+        System.out.printf("Digite o numero da agencia do cliente: ");
+        numero = sc.nextInt();
+        System.out.printf("Digite o saldo do cliente: ");
+        saldo = sc.nextDouble(); 
+
+        System.out.printf("%nOlá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.%n",nomeCliente, agencia, numero, saldo);
+
+        sc.close();
+    }
+    
+    
+}
