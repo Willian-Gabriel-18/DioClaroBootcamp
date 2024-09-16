fd3a3c8c36e23698e39f6464fa3143732e9a9676
will <willpro2806@gmail.com>
2024-09-16 10:02:58 -0300

    Resolução do segundo desafio de código

---
+++ DesafioDeCodigo_SintaxeBasicaJava/ServicoComboCompleto/src/edu/will/servico/Main.java
@@ -0,0 +1,55 @@
+package edu.will.servico;
+
+import java.util.Scanner;
+
+public class Main {
+
+    // Função para verificar se o cliente contratou um combo completo
+    public static String verificarComboCompleto(String[] servicosContratados) {
+        // Variáveis booleanas para verificar a contratação de cada serviço
+        boolean movelContratado = false;
+        boolean bandaLargaContratada = false;
+        boolean tvContratada = false;
+
+        // Iterar sobre os serviços contratados
+        // móvel, banda larga e tv - COMBO COMPLETO
+        for (String servico : servicosContratados) {
+            if(servico.trim().equals("movel")){
+                movelContratado = true;
+            }
+            if(servico.trim().equals("banda larga")){
+                bandaLargaContratada = true;
+            }
+            if(servico.trim().equals("tv")){
+                tvContratada = true;
+            }
+        }
+
+        // Verificar se todos os serviços foram contratados
+        if (movelContratado && bandaLargaContratada && tvContratada) {
+            return "Combo Completo";
+        } else {
+            return "Combo Incompleto";
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicitando ao usuário a lista de serviços contratados
+        System.out.printf("Lista de servicos contratados: ");
+        String input = scanner.nextLine();
+
+        // Convertendo a entrada em uma lista de strings
+        String[] servicosContratados = input.split(",");
+
+        // Verificando se o cliente contratou um combo completo
+        String resultado = verificarComboCompleto(servicosContratados);
+
+        // Exibindo o resultado
+        System.out.println(resultado);
+
+        // Fechando o scanner
+        scanner.close();
+    }
+}
