8c7546aa692b06700e3c11022bdc666452675570
will <willpro2806@gmail.com>
2024-09-16 09:34:24 -0300

    Verificacao Servico concluida

---
+++ ServicoTelefonia/src/edu/will/servico/VerificacaoServico.java
@@ -0,0 +1,38 @@
+package edu.will.servico;
+
+import java.util.Scanner;
+
+public class VerificacaoServico {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        // Entrada do serviço a ser verificado
+        System.out.printf("Serviço a ser verificado: ");
+        String servico = scanner.nextLine().trim();
+        
+        // Entrada do nome do cliente e os serviços contratados
+        System.out.printf("Nome cliente e serviços contratados: ");
+        String entradaCliente = scanner.nextLine().trim();
+        
+        // Separando o nome do cliente e os serviços contratados
+        String[] partes = entradaCliente.split(",");
+        String nomeCliente = partes[0];
+        boolean contratado = false;
+        
+        // TODO: Verifique se o serviço está na lista de serviços contratados
+        for(String p : partes){
+            if(p.trim().equals(servico)){
+              contratado = true;
+              break;
+            }
+        }
+          
+        if(contratado == true){
+        System.out.println("Sim");
+        }else{
+        System.out.println("Nao");
+        }
+
+        scanner.close();
+    }
+}
